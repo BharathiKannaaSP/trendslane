050c1481c067b85dae6b6c3a5aea3d1ae460798a
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2025-11-08 17:55:16 +0530

    Fetch whole history github

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HomePage = () => {
-	return <h1>------------ Admin Home CI CD check ----------</h1>
+	return <h1>------------ Admin Home CI CD check ---------</h1>
 }
 
 export default HomePage
