25e028b960d19b25009a53c722887430ad803d3b
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2025-11-08 17:59:33 +0530

    test(ci): check only redeploys

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const HomePage = () => {
-	return <h1>------------ Admin Home CI CD check ---------</h1>
+	return (
+		<h1>------------ Admin Home CI CD check --------- Hello CI?CD Check</h1>
+	)
 }
 
 export default HomePage
