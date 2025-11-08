4ac6a7e2eafa7c2fba5d590f2b0443aed8e9aab6
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2025-11-08 19:30:25 +0530

    Admin changes

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const HomePage = () => {
-	const hello = ''
+	const hello = 'Bharathi'
+	console.log(hello)
 	return (
-		<h1>------------ Admin Home CI CD check --Hello--- ---- Hello CI////CD Check</h1>
+		<h1>
+			------------ Admin Home CI CD check --Hello--- ---- Hello CI////CD Check
+		</h1>
 	)
 }
 
