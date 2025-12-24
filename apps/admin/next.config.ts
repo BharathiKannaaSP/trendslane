7fd68491d688dc1a9cb78bb2abe279669391f311
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2025-12-24 16:33:44 +0530

    Added admin app

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        hostname: 'shop.mango.com',
+      },
+      {
+        hostname: 'images.pexels.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
