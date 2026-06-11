0fdc2f6e45f663f4fcd3201e75f0bb2900aa7f3c
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2026-06-11 16:56:28 +0530

    Added github action for deploy

--- packages/shared/src/formatters/time-formats.ts
+++ packages/shared/src/formatters/time-formats.ts
@@ -1,9 +1,10 @@
-// Get Date - 10-Jun-2026, Time - 21:15
+// Get Date - 10-Jun-2026, Time - 15:45 UTC
 export function getCurrentTimestamp() {
   const now = new Date()
 
   const date = now
     .toLocaleDateString("en-GB", {
+      timeZone: "UTC",
       day: "2-digit",
       month: "short",
       year: "numeric",
@@ -11,10 +12,11 @@ export function getCurrentTimestamp() {
     .replace(/ /g, "-")
 
   const time = now.toLocaleTimeString("en-GB", {
+    timeZone: "UTC",
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
   })
 
-  return `Date - ${date}, Time - ${time}`
+  return `Date - ${date}, Time - ${time} UTC`
 }
